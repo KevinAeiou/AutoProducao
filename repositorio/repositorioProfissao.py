dd9572067cd7fc6e78e8c9e131d6aad1f7377b55
KevinAeiou <kevin.souzaiou@outlook.com>
2024-10-16 23:42:27 -0400

    Implementar removeProfissao e limpaProfissoes

--- repositorio/repositorioProfissao.py
+++ repositorio/repositorioProfissao.py
@@ -41,6 +41,22 @@ class RepositorioProfissao:
         except Exception as e:
             self.__erro = str(e)
         return False
+    
+    def removeProfissao(self, profissao):
+        try:
+            self.__meuBanco.child(CHAVE_USUARIOS).child(CHAVE_ID_USUARIO).child(CHAVE_LISTA_PERSONAGEM).child(self.__personagem.pegaId()).child(CHAVE_LISTA_PROFISSAO).child(profissao.pegaId()).remove()
+            return True
+        except Exception as e:
+            self.__erro = str(e)
+        return False
+    
+    def limpaProfissoes(self):
+        try:
+            self.__meuBanco.child(CHAVE_USUARIOS).child(CHAVE_ID_USUARIO).child(CHAVE_LISTA_PERSONAGEM).child(self.__personagem.pegaId()).child(CHAVE_LISTA_PROFISSAO).remove()
+            return True
+        except Exception as e:
+            self.__erro = str(e)
+        return False
 
     def pegaErro(self):
         return self.__erro
