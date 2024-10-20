31364147be44c5ba2de7c8094c116d471ed55337
KevinAeiou <kevin.souzaeiou@outlook.com>
2024-10-20 15:41:08 -0400

    Atualizar classe TrabalhoVendido

--- modelos/trabalhoVendido.py
+++ modelos/trabalhoVendido.py
@@ -12,36 +12,6 @@ class TrabalhoVendido:
         self.trabalhoId = None
         self.valorProduto = None
 
-    def pegaId(self):
-        return self.id
-    
-    def pegaNome(self):
-        return self.nomeProduto
-    
-    def pegaDataVenda(self):
-        return self.dataVenda
-    
-    def pegaNomePersonagem(self):
-        return self.nomePersonagem
-    
-    def pegaQuantidadeProduto(self):
-        return self.quantidadeProduto
-    
-    def pegaTrabalhoId(self):
-        return self.trabalhoId
-    
-    def pegaValorProduto(self):
-        return self.valorProduto
-    
-    def setId(self, id):
-        self.id = id
-
-    def setNome(self, nome):
-        self.nomeProduto = nome
-
-    def setData(self, data):
-        self.dataVenda = data
-
     def setQuantidade(self, quantidade):
         quantidade = 0 if int(quantidade) < 0 else int(quantidade)
         self.quantidadeProduto = quantidade
@@ -55,4 +25,5 @@ class TrabalhoVendido:
             setattr(self, chave, dicionario[chave])
             
     def __str__(self) -> str:
-        return f'{(self.nomeProduto).ljust(113)} | {(self.dataVenda).ljust(10)} | {(self.trabalhoId).ljust(36)} | {str(self.valorProduto).ljust(5)} | {self.quantidadeProduto}'
+        nome = '' if self.nomeProduto == None else self.nomeProduto
+        return f'{(nome).ljust(113)} | {(self.dataVenda).ljust(10)} | {(self.trabalhoId).ljust(36)} | {str(self.valorProduto).ljust(5)} | {self.quantidadeProduto}'
