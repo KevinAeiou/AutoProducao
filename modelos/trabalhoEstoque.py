0ec8defeb1b31002182033e5b3ede8fef815e318
Kevin <kevin.souzaeiou@outlook.com>
2025-01-13 11:20:39 -0400

    :recycle: refactor: função setQuantidade, __str__

--- modelos/trabalhoEstoque.py
+++ modelos/trabalhoEstoque.py
@@ -9,7 +9,8 @@ class TrabalhoEstoque(Trabalho):
         self.quantidade = 0
     
     def setQuantidade(self, quantidade):
-        self.quantidade = int(quantidade)
+        quantidade = 0 if int(quantidade) < 0 else int(quantidade)
+        self.quantidade = quantidade
     
     def dicionarioParaObjeto(self, dicionario):
         for chave in dicionario:
@@ -19,4 +20,4 @@ class TrabalhoEstoque(Trabalho):
         id = 'Indefinido' if self.id == None else str(self.id)
         trabalhoId = 'Indefinido' if self.trabalhoId == None else str(self.trabalhoId)
         quantidade = str(self.quantidade)
-        return f'{id.ljust(36)} | {trabalhoId.ljust(36)} | {quantidade}'
+        return f'{id.ljust(36)} | {trabalhoId.ljust(36)} | {quantidade.ljust(3)}'
