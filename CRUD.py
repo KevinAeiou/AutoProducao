632c52bf938ab869c3d3f2d628a699cc3b90857a
KevinAeiou <kevin.souzaeiou@outlook.com>
2025-01-29 16:44:19 -0300

    :sparkles: refactor: função iniciaStreans

--- CRUD.py
+++ CRUD.py
@@ -846,10 +846,12 @@ class CRUD:
             continue
         input('Clique para continuar...')
         
+    def iniciaStreans(self):
+        self.__aplicacao.abreStreamTrabalhos()
+        self.__aplicacao.abreStreamPersonagens()
+        
     def testeFuncao(self):
-        # self.__aplicacao.abreStreamPersonagens()
-        while True:
-            self.__aplicacao.retornaMenu()
+        self.__aplicacao.abreStreamPersonagens()
         
     def menu(self):
         while True:
@@ -876,6 +878,7 @@ class CRUD:
             print(f'{"17".ljust(2)} - Remove trabalho vendido')
             print(f'{"20".ljust(2)} - Pega todos trabalhos producao')
             print(f'{"21".ljust(2)} - Sincroniza dados')
+            print(f'{"22".ljust(2)} - Inicia streans')
             print(f'{"24".ljust(2)} - Teste de funções')
             print(f'{"0".ljust(2)} - Sair')
             try:
@@ -938,6 +941,9 @@ class CRUD:
                     continue
                 if int(opcaoMenu) == 21:
                     self.sincronizaDados()
+                    continue
+                if int(opcaoMenu) == 22:
+                    self.iniciaStreans()
                     continue
                 if int(opcaoMenu) == 24:
                     self.testeFuncao()
