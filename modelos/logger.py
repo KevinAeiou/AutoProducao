0b5dbe32bd6e963b82817d48502f485309185d3c
KevinAeiou <kevin.souzaeiou@outlook.com>
2025-04-03 13:22:24 -0400

    :sparkles: feat: Atualizar classe MeuLogger para tratar error de permissão

--- modelos/logger.py
+++ modelos/logger.py
@@ -1,6 +1,8 @@
 import logging
 import os
+from pathlib import Path
 from logging.handlers import RotatingFileHandler
+import tempfile
 
 class MeuLogger:
     def __init__(self, nome: str, arquivoLogger: str= 'aplicacao.log', nivel: int = logging.DEBUG, bytesMax: int=10485760, contadorBackup: int= 5):
@@ -8,17 +10,33 @@ class MeuLogger:
         self.__logger.setLevel(level= nivel)
         formatador = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
         formatador.datefmt = '%d/%m/%Y %I:%M:%S %p'
-        caminhoLogger: str= 'logs'
-        arquivoLogger= caminhoLogger + '/' + arquivoLogger
-        if not os.path.exists(arquivoLogger):
-            os.makedirs(caminhoLogger)
-            with open(arquivoLogger, 'w', encoding= 'utf-8') as arquivo:
-                arquivo.write('')
-        if self.__logger.handlers:
-            self.__logger.handlers.clear()
-        file_handler = RotatingFileHandler(arquivoLogger, maxBytes= bytesMax, backupCount=contadorBackup, encoding= 'utf-8')
-        file_handler.setFormatter(formatador)
-        self.__logger.addHandler(file_handler)
+        if arquivoLogger:
+            try:
+                caminhoLogger: str= 'logs'
+                arquivoLogger= caminhoLogger + '/' + arquivoLogger
+                if not os.path.exists(arquivoLogger):
+                    os.makedirs(arquivoLogger)
+                    with open(arquivoLogger, 'w', encoding= 'utf-8') as arquivo:
+                        arquivo.write('')
+                if self.__logger.handlers:
+                    self.__logger.handlers.clear()
+                file_handler = RotatingFileHandler(arquivoLogger, maxBytes= bytesMax, backupCount=contadorBackup, encoding= 'utf-8')
+                file_handler.setFormatter(formatador)
+                self.__logger.addHandler(file_handler)
+            except PermissionError as e:
+                temp_log = Path(tempfile.gettempdir()) / 'AutoProducao' / arquivoLogger
+                temp_log.parent.mkdir(parents=True, exist_ok=True)
+                if self.__logger.handlers:
+                    self.__logger.handlers.clear()
+                file_handler = RotatingFileHandler(
+                    str(temp_log),
+                    maxBytes=bytesMax,
+                    backupCount=contadorBackup,
+                    encoding='utf-8'
+                )
+                file_handler.setFormatter(formatador)
+                self.__logger.addHandler(file_handler)
+
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatador)
         self.__logger.addHandler(console_handler)
